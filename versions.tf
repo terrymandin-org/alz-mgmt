cc350e4f354253ab81a7fe3dbbc764cf56bc477d
terry.mandin@microsoft.com <terry.mandin@microsoft.com>
2023-11-20 09:25:16 -0700

    Add versions.tf [skip ci]

---
+++ versions.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    azurerm = ">= 3.0.0"
+  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
+
+provider "azurerm" {
+  alias                      = "management"
+  subscription_id            = var.subscription_id_management
+  skip_provider_registration = true
+  features {}
+}
+
+provider "azurerm" {
+  alias                      = "connectivity"
+  subscription_id            = var.subscription_id_connectivity
+  skip_provider_registration = true
+  features {}
+}
