1cdbda361f5057038b34e9ed437349b426b17fa8
terry.mandin@microsoft.com <terry.mandin@microsoft.com>
2023-11-20 09:25:10 -0700

    Add variables.tf [skip ci]

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "default_location" {
+  description = "The location for Azure resources. (e.g 'uksouth')|1|azure_location"
+  type        = string
+}
+
+variable "subscription_id_connectivity" {
+  description = "The identifier of the Connectivity Subscription. (e.g '00000000-0000-0000-0000-000000000000')|2|azure_subscription_id"
+  type        = string
+}
+
+variable "subscription_id_identity" {
+  description = "The identifier of the Identity Subscription. (e.g '00000000-0000-0000-0000-000000000000')|3|azure_subscription_id"
+  type        = string
+}
+
+variable "subscription_id_management" {
+  description = "The identifier of the Management Subscription. (e.g 00000000-0000-0000-0000-000000000000)|4|azure_subscription_id"
+  type        = string
+}
+
+variable "root_id" {
+  description = "The root id is the identity for the root management group and a prefix applied to all management group identities|5|azure_name"
+  type        = string
+  default     = "es"
+}
+
+variable "root_name" {
+  description = "The display name for the root management group|6|azure_name"
+  type        = string
+  default     = "Enterprise-Scale"
+}
